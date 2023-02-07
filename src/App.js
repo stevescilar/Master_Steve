f0f614d67e1cb93f0d88355f6f04868cf4fa10fe
stevescilar <stevemuambi@gmail.com>
2023-02-07 14:55:57 +0300

    home component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <>
     <Header />
+
+    <main className='main'>
+      <Home />
+    </main>
     </>
+
+   
   );
 }
 
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i> 
+        </a>
+
+        <a href="https://github.com" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://twitter.com" className="home__social-icon" target="_blank">
+        <i class="uil uil-twitter-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social

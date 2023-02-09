9e384293dbb32f6393e183075c3f3cdba60fc73c
stevescilar <stevemuambi@gmail.com>
2023-02-09 23:29:21 +0300

    update-skills

---
+++ src/components/skills/Frontend.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Frontend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title"></h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">
+                            <span className="skills__level">
+
+                            </span>
+                        </h3>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">
+                            <span className="skills__level">
+
+                            </span>
+                        </h3>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">
+                            <span className="skills__level">
+
+                            </span>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Frontend

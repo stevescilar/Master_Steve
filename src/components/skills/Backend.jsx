e0f6752a9d9a19a5c73c9277890349a0f51b2c19
stevescilar <stevemuambi@gmail.com>
2023-02-09 23:44:50 +0300

    update-skills

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,47 +3,64 @@ import React from 'react'
 const Backend = () => {
   return (
     <div className="skills__content">
-        <h3 className="skills__title"></h3>
+        <h3 className="skills__title">Frontend Developer</h3>
 
-            <div className="skills__box">
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name">
-                                <span className="skills__level">
-
-                                </span>
-                            </h3>
-                        </div>
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name"> HTML </h3>
+                        <span className="skills__level"> Advanced </span>
                     </div>
+                </div>
 
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name">
-                                <span className="skills__level">
-
-                                </span>
-                            </h3>
-                        </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name"> CSS </h3>
+                        <span className="skills__level"> Advanced</span>
                     </div>
+                </div>
 
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name">
-                                <span className="skills__level">
-
-                                </span>
-                            </h3>
-                        </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Javascript</h3>
+                            <span className="skills__level">
+                                Advanced
+                            </span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">ReactJS</h3>
+                            <span className="skills__level">
+                                Intermediate
+                            </span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">VueJS</h3>
+                            <span className="skills__level">
+                                Intermediate
+                            </span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Bootstrap</h3>
+                            <span className="skills__level">
+                                Advanced
+                            </span>
                     </div>
                 </div>
             </div>
+        </div>
     </div>
   )
 }

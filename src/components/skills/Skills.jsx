f4ec912d79c26d3af975eb98edca966b83e97eaf
stevescilar <stevemuambi@gmail.com>
2023-02-09 23:14:38 +0300

    update

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills Galore</div>
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">I am good at</span>
+    </section>
   )
 }
 

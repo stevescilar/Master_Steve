804b484d4bd8c7a0b9a7d2cc89e2a880175e587e
stevescilar <stevemuambi@gmail.com>
2023-02-11 13:42:26 +0300

    qualifications section-stopped at here

--- src/components/qualifications/Qualifications.jsx
+++ src/components/qualifications/Qualifications.jsx
@@ -20,7 +20,7 @@ const Qualifications = () => {
             <div className="qualification__content">
                 <div className="qualification__data">
                     <div>
-                        <h3 className="qualification__title"></h3>
+                        <h3 className="qualification__title">Web Trainer</h3>
                         <span className="qualification__subtitle">JKUAT</span>
                     </div>
                     <div className="qualification__calendar">
@@ -29,8 +29,135 @@ const Qualifications = () => {
 
                     <div>
                         <span className="qualification__rounder"></span>
-                        <div className="qualification__line"></div>
+                        <span className="qualification__line"></span>
                     </div>
+                </div>
+
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification__title">Tutorial Fellow</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2014-2017
+                    </div>
+
+                    
+                </div>
+
+                <div className="qualification__data">
+                    <div>
+                        <h3 className="qualification__title">Web Development</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2014-2017
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification__title">UX/UI Expert</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2016-2018
+                    </div>
+
+                    
+                </div>
+            </div>
+
+            <div className="qualification__content">
+                <div className="qualification__data">
+                    <div>
+                        <h3 className="qualification__title">Web Trainer</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2014-2017
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification__title">Tutorial Fellow</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2014-2017
+                    </div>
+
+                    
+                </div>
+
+                <div className="qualification__data">
+                    <div>
+                        <h3 className="qualification__title">Web Development</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2014-2017
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification__title">UX/UI Expert</h3>
+                        <span className="qualification__subtitle">JKUAT</span>
+                    </div>
+                    <div className="qualification__calendar">
+                        <i className="uil uil-calendar-alt"></i>2016-2018
+                    </div>
+
+                    
                 </div>
             </div>
         </div>

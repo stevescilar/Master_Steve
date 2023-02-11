c81c34debe0fe3c2d8d5a48568b422fa75629e27
stevescilar <stevemuambi@gmail.com>
2023-02-11 12:39:57 +0300

    qualifications section

--- src/components/qualifications/Qualifications.jsx
+++ src/components/qualifications/Qualifications.jsx
@@ -4,7 +4,10 @@ import "./qualifications.css";
 
 const Qualifications = () => {
   return (
-    <div>Qualifications</div>
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__title">The Path i have taken</span>
+    </section>
   )
 }
 
